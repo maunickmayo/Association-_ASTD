a38e47c981f96786c8df54cbb60d219006529be4
Maunick MAYO <104192198+maunickmayo@users.noreply.github.com>
2024-09-26 13:32:41 +0200

    Update UserController.php

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -34,7 +34,6 @@ class UserController extends AbstractController
     public function showProfile(EntityManagerInterface $entityManager): Response
     {  
         $articles = $entityManager->getRepository(Article::class)->findBy(['author' => $this->getUser()]);
-        // on veut afficher ici les articles publiés , NB: l'admin est aussi un utilisateur donc il a aussi un profil
         return $this->render('user/show_profile.html.twig', [
             'articles' => $articles
         ]);
@@ -46,13 +45,11 @@ class UserController extends AbstractController
     */
     public function editProfile(User $user, Request $request, EntityManagerInterface $entityManager): Response
     {
-        // $user = $this->getUser();  pour recup les infos de user
         $form = $this->createForm(EditProfileFormType::class, $user)
         ->handlerequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-     // si le formulaire a été envoyé et valide, isValid (booléen) va aller voir s'il y'a des erreurs.
-            $user->setUpdatedAt(new DateTime()); // pr que la date de modification soit differente de celle de creation.
+            $user->setUpdatedAt(new DateTime());
 
             $entityManager->persist($user);
             $entityManager->flush();
@@ -74,21 +71,16 @@ class UserController extends AbstractController
     */
     public function editPassword(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
-        //NB: change password peut être mis ici ou a SecurityController (puisqu'il s'git ici d 'une action de sécurité).
-        // rappel un DELIMITER ($) est un opération de fin d'instruction ( exp le ;)
 
         $form = $this->createForm(ChangePasswordFormType::class)
               ->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             /** @var User $user  */
-            // qui sert a typer pour l'auto-completion , pr que l ide sache que $user c est pour UserEntity , pour que lorsqu'on fait une fêlche pour acceder aux methodes il nous liste tts les methodes qu'il y ' ds USER.
             $user = $entityManager->getRepository(User::class)->findOneBy(['id' => $this->getUser()]);
 
             $user->setUpdatedAt(new Datetime());
             $user->setPassword($passwordHasher->hashPassword($user, $form->get('plainPassword')->getData()));
-
-            // get('plainPassword') inout en entier mais quand on met get('plainPassword')->getData() on veut les données qui st à l'interieur. plainPassword =>  veut dire mdp en clair.
 
             $entityManager->persist($user);
             $entityManager->flush();
@@ -118,14 +110,10 @@ class UserController extends AbstractController
             $photo = $form->get('photo')->getData();
 
             if ($photo) {
-                //-------------// et  tte cette partie c est pour creer le nom du fichier------------------
                 $originalFilename = pathinfo($photo->getClientOriginalName(), PATHINFO_FILENAME);
-                // this is needed to safely include the file name as part of the URL
-                $safeFilename = $slugger->slug($originalFilename);// il cree un slug associé à l'original name, pour eviter les noms commens ds la BDD; le slug c est un service loader;
-                //uniqid() : un id unique qui sera genere au moment ou l'on appelle la méthode.
-
+               
+                $safeFilename = $slugger->slug($originalFilename);
                 $newFilename = $safeFilename.'-'.uniqid().'.'.$photo->guessExtension();
-                // il va aller copier le contenu temporaire du ficher qu'on va uploader pour aller le stocker qqlpart ds notre appli.
                 try {
                     $photo->move(
                         $this->getParameter('uploads_dir'),
@@ -134,7 +122,6 @@ class UserController extends AbstractController
                 } catch (FileException $e) {
                    
                 }
-                //et si tout se passe bien on lui passe le newfilename apres avoir pu l'uploader au niveau de notre appli.
                 $user->setPhoto($newFilename);
             }
             $entityManager->persist($user);
@@ -173,35 +160,25 @@ class UserController extends AbstractController
 
               /** @var UploadedFile $photo */
               $photo = $form->get('photo')->getData();
-            // this condition is needed because the 'brochure' field is not required
-            // so the PDF file must be processed only when a file is uploaded
+            
             if ($photo) {
-                //-------------// et  tte cette partie c est pour creer le nom du fichier------------------
                 $originalFilename = pathinfo($photo->getClientOriginalName(), PATHINFO_FILENAME);
-                // this is needed to safely include the file name as part of the URL
-                $safeFilename = $slugger->slug($originalFilename);// il cree un slug associé à l'original name, pour eviter les noms commens ds la BDD; le slug c est un service loader;
-                //uniqid() : un id unique qui sera genere au moment ou l'on appelle la méthode.
+              
+                $safeFilename = $slugger->slug($originalFilename);
 
                 $newFilename = $safeFilename.'-'.uniqid().'.'.$photo->guessExtension();
-
-                // Move the file to the directory where brochures are stored
-                // il va aller copier le contenu temporaire du ficher qu'on va uploader pour aller le stocker qqlpart ds notre appli.
+                
                 try {
                     $photo->move(
                         $this->getParameter('uploads_dir'),
                         $newFilename);
                 } catch (FileException $e) {
                     // ... handle exception if something happens during file upload
-                    // echo 'Impossible d\'enregistrer l\'image';
                 }
 
-                // updates the 'brochureFilename' property to store the PDF file name
-                // instead of its contents
-                //et si tout se passe bien on lui passe le newfilename apres avoir pu l'uploader au niveau de notre appli.
                 $user->setPhoto($newFilename);
             }
 
-            //$user->setPassword($passwordHasher->hashPassword($user, $user->getPassword() ));
             $entityManager->persist($user);
             $entityManager->flush();
 
@@ -236,10 +213,8 @@ class UserController extends AbstractController
           if ($currentsession){
            
             $session->invalidate();
-            $entityManager->remove($user);
-           // $this->addFlash('message', 'Votre profil a bien été supprimé.');   
+            $entityManager->remove($user); 
             $entityManager->flush();  
-           
            }
            $this->addFlash('message', 'Votre profil a bien été supprimé.'); 
            return $this->redirectToRoute('app_register'); 
